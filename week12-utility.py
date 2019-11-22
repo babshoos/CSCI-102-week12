fc7180036c7ae138027e806277be2ba6421d1d6a
Bum Soo Kim <bumsookim@mines.edu>
2019-11-21 19:31:14 -0700

    created file that find word count

--- week12-utility.py
+++ week12-utility.py
@@ -1,4 +1,9 @@
-def PrintOutout(word):
+#
+#Bumsoo Kim
+#CSCI102- Sec E
+#Week 12 part B
+
+def PrintOutput(word):
     print(word)
 def LoadFile(file):
     with open (file, "r") as f:
@@ -12,4 +17,56 @@ def UpdateString(string, sub, loc):
     print("OUTPUT ", end='')
     for z in range(len(li)):
         print(li[z], end='')
-        
+def FindWordCount(a, word):
+    num = 0
+    for n in range (len(a)):
+        if a[n] == word:
+            num +=1
+    return num
+            
+def ScoreFinder(players, scores, name):
+    loc = 0
+    tof = len(players)
+    name = name.upper()
+    for m in range(len(players)):
+        players[m] = players[m].upper()
+        if players[m] == name:
+            loc = m
+            tof = tof **2
+            
+        else:
+            tof -= 1
+            
+    sc = scores[loc]
+    if tof == 0:
+        print("OUTPUT player not found")
+    else:
+        print ("OUTPUT ", name, " got a score of ", sc)
+
+def Union(scores, players2):
+    for c in players2:
+        scores.append(c)
+    return scores
+def Intersection(players, players2):
+    nli=[]
+    for q in players:
+        for p in players2:
+            if p == q:
+                nli.append(q)
+    return nli
+def NotIn(players2, players):
+    rli=[]
+    blis=players2
+    for v in blis:
+        for w in players:
+            if w == v:
+                rli.append(v)
+    for r in range(len(rli)):
+        blis.remove(rli[r])
+    print(t1, t2)
+    return blis
+
+                
+                
+    
+    
